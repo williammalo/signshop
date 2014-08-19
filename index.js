8e209ddd6a8c9a736b5edd2bf06d48f23a56074a
William Malo <will@a.local>
2014-08-19 11:58:14 -0400

    added static content, removed deferred font loading

--- index.js
+++ index.js
@@ -246,14 +246,4 @@ inputFormElement.on("submit", (function(e) {
   history.pushState("", "", "?search=" + SSH.inputElement.value);
   e.preventDefault();
 }));
-SSH.inputElement.value = getQueryVariable("search");
-SSH.search();
-var cb = function() {
-  var l = document.createElement('link');
-  l.rel = 'stylesheet';
-  l.href = 'http://fonts.googleapis.com/css?family=Ubuntu:400,700';
-  var h = document.getElementsByTagName('head')[0];
-  h.parentNode.insertBefore(l, h);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(cb); else window.addEventListener('load', cb);
+if (SSH.inputElement.value = getQueryVariable("search")) SSH.search();
