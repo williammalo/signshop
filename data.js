24ebe18241b48c9eb0ae480fec4c686d929557cd
William Malo <williammalo2@gmail.com>
2015-11-30 11:31:38 -0500

    added model

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 WS.data=[
+["304dde38","GMC Sierra\n/ Chevrolet Silverado crew‑cab 3500 HD long-box 2014‑uptodate","pickup",68,"00608"],
 ["29185114","Mercedes Metris cargo van 2016‑uptodate","van",82,"00607"],
 ["6751b7bd","Ford F‑150\nsupercrew 5.5ft 2015‑uptodate","pickup",75,"00606"],
 ["a2f1142e","Ford F‑150\nsupercrew 6.5ft 2015‑uptodate","pickup",78,"00605"],
